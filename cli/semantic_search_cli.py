09c50a50571c6f7834cc406ee2f83d20526210b6
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2025-12-07 22:24:56 +0200

    embedding models

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
 import argparse
+from lib.semantic_search import *
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
-    args = parser.parse_args()
+    subparsers = parser.add_subparsers(dest="command", help="Available commands") 
 
+    verify_parser = subparsers.add_parser('verify', help='Verify embedding model')
+    
+
+    args = parser.parse_args()
+    
     match args.command:
-        case _:
-            parser.print_help()
+        case "verify": return verify_command()
+        case _: parser.print_help()
 
 if __name__ == "__main__":
     main()
